47f86acc22f835747147d37bb515ea78592a8d08
Jonathon Penix <jpenix@quicinc.com>
2025-12-07 01:49:49 -0800

    WIP: MUSL first draft
    Needs fiddling with libc++ and friends and general cleanup.
    
    Think directly invoking the build scripts makes more sense than trying
    to untangle the 'multiple builds per config' thing we have going on. If
    we have to do MUSL for RISC-V, that might change.
    
    Signed-off-by: Jonathon Penix <jpenix@quicinc.com>

--- arm-software/embedded/cmake/generate_version_txt.cmake
+++ arm-software/embedded/cmake/generate_version_txt.cmake
@@ -20,6 +20,8 @@ endif()
 if(NOT (LLVM_TOOLCHAIN_C_LIBRARY STREQUAL llvmlibc)) # libc in a separate repo?
     if(LLVM_TOOLCHAIN_C_LIBRARY MATCHES "^newlib")
         set(base_library newlib)
+    elseif(LLVM_TOOLCHAIN_C_LIBRARY STREQUAL musl-embedded)
+        set(base_library musl)
     else()
         set(base_library ${LLVM_TOOLCHAIN_C_LIBRARY})
     endif()
