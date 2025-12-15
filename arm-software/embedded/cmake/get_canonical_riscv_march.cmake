fc29f1eb29c4ec761aca8f3331a9995a34a35c4b
Jonathon Penix <jpenix@quicinc.com>
2025-12-15 11:19:47 -0800

    Use `--print-enabled-extensions` to get the canonical `-march` string
    Turns out this option gives the canonical arch string, and this is
    marginally simpler than compiling a simple C file and parsing
    the llvm-readelf output.
    
    Signed-off-by: Jonathon Penix <jpenix@quicinc.com>

--- arm-software/embedded/cmake/get_canonical_riscv_march.cmake
+++ arm-software/embedded/cmake/get_canonical_riscv_march.cmake
@@ -1,6 +1,5 @@
 # Retrieve the canonical `-march` string given a set of valid compiler flags.
-# We do this by compiling a simple C file and parsing llvm-readelf's output
-# to get the canonical arch string.
+# We do this by consulting clang's `--print-enabled-extensions` output.
 # 
 # We require the following arguments:
 # - `compiler_path` is expected to contain the path to the compiler to use to
@@ -12,32 +11,17 @@
 # - `march_out` should contain the variable to be used to return the
 #   canonicalized arch string.
 function(get_canonical_riscv_march compiler_path build_args march_out)
-    set(simple_source ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/simple.c)
-    file(WRITE ${simple_source} "int main(void) {}\n")
-
-    set(simple_obj_file ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/simple.o)
-    set(command_args ${simple_source} ${build_args} "-c" "-o" ${simple_obj_file})
+    set(command_args ${build_args} "--print-enabled-extensions")
     execute_process(
         COMMAND ${compiler_path}
         ${command_args}
         RESULT_VARIABLE return_val
+        OUTPUT_VARIABLE extension_output
     )
     if(NOT return_val EQUAL 0)
-        message(FATAL_ERROR "Unable to compile C file to normalize `-march` string")
-    endif()
-    execute_process(
-        COMMAND ${LLVM_BINARY_DIR}/bin/llvm-readelf${CMAKE_EXECUTABLE_SUFFIX}
-        -A ${simple_obj_file}
-        RESULT_VARIABLE return_val
-        OUTPUT_VARIABLE readelf_output
-    )
-    if(NOT return_val EQUAL 0)
-        message(FATAL_ERROR "Unable retrieve canonicalized `-march` string from llvm-readelf")
+        message(FATAL_ERROR "Unable to execute `--print-enabled-extensions` to retreive canonical `-march` string")
     endif()
     string(REGEX MATCH
-            "Tag: 5[ \t\r\n]+TagName: arch[ \t\r\n]+Value: ([A-Za-z0-9_]+)"
-            out_var "${readelf_output}"
-    )
-
+            "ISA String: ([A-Za-z0-9_]+)" out_var "${extension_output}")
     set(${march_out} ${CMAKE_MATCH_1} PARENT_SCOPE)
 endfunction()
